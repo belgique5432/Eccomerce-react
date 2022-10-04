5ff700ec55cf0bf573be453e1b0ab6a1a8700ba9
belgique5432 <seniniezequiel780@gmail.com>
2022-10-03 23:17:15 -0300

    nuevos cambios sesion2

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 import { useCartContext } from '../../context/CartContext';
 import {Link} from 'react-router-dom';
 import ItemCart from '../ItemCart/ItemCart';
+import { addDoc, getFirestore, collection } from 'firebase/firestore';
 
 const Cart = () => {
     const {cart, totalPrice} = useCartContext();
+
+    const order = {
+        buyer : {
+            name: 'Pablo',
+            email: 'Pablo@gmail.com',
+            phone: '123123',
+            adress: 'asdd'
+        },
+        items: cart.map(product => ({ id: product.id, title:product.title, price: product.price, quantity:product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(({ id } ) => console.log(id)) 
+    }
 
     if (cart.length === 0){
         return (
@@ -19,6 +38,10 @@ const Cart = () => {
         {
             cart.map(product => <ItemCart key = {product.id} product={product }/>)
         }
+        <p>
+            total: {totalPrice()}
+        </p>
+        <button onClick={handleClick}>Emitir compra</button>
         </>
     )
 
